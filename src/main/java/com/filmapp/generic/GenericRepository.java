47f236b05874a2e28216274d77fddee359016f20
AKincel18 <akincel97@gmail.com>
2022-01-30 20:11:57 +0100

    removed generic endpoint

--- src/main/java/com/filmapp/generic/GenericRepository.java
+++
@@ -1,7 +0,0 @@
-package com.filmapp.generic;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-/*public interface GenericRepository<Entity extends BaseEntity> extends JpaRepository<Entity, Long> {
-    //Entity getByName(String name);
-}*/
